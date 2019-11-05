d61f97666c10e6d5c5ace6c81a446777360641a3
Taras <50269675+TarasHryts@users.noreply.github.com>
2019-11-05 09:32:22 +0200

    Delete Representation.java

--- src/main/java/Representation.java
+++
@@ -1,9 +0,0 @@
-public class Representation {
-    public static void main(String[] args) {
-        Counter counter = new Counter();
-        MyNewThread myNewThread = new MyNewThread(counter);
-        Runner runner = new Runner(counter);
-        myNewThread.start();
-        new Thread(runner).start();
-    }
-}
